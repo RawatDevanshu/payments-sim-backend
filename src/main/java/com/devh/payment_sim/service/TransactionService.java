2e51532d884d8416b7afa4121227d1f4de4297e4
DevanshuRawat-cpg <DEVANSHU.RAWAT@CAPGEMINI.COM>
2026-03-05 17:27:29 +0530

    feat(Pagination): implement pagination and sort for all transaction

--- src/main/java/com/devh/payment_sim/service/TransactionService.java
+++ src/main/java/com/devh/payment_sim/service/TransactionService.java
@@ -1,7 +1,9 @@
 package com.devh.payment_sim.service;
 
 import java.math.BigDecimal;
-import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import com.devh.payment_sim.model.Transaction;
 
@@ -9,5 +11,5 @@ public interface TransactionService {
     Transaction sendMoney(String fromUpi, String toUpi, BigDecimal amount, String upiPin, String remarks);
     Transaction topUpFromBank(Long userId, String walletUpiHandle, String bankAccountNumber, BigDecimal amount, String bankPin, String remarks);
     Transaction withdrawFromWallet(Long userId, String walletUpiHandle, String bankAccountNumber, BigDecimal amount, String walletPin, String remarks);
-    List<Transaction> getTransactionsByWalletUpi(String walletUpiHandle);
+    Page<Transaction> getTransactionsByWalletUpi(String walletUpiHandle, Pageable pageable);
 }

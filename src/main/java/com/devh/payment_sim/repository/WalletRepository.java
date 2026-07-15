71c31944db81c6d3e52fd8bcd42937811d50ff70
RawatDevanshu <rawatdevanshu22@gmail.com>
2026-07-15 06:00:45 +0530

    feat(Concurrency): implement pessimistic locking and tests

--- src/main/java/com/devh/payment_sim/repository/WalletRepository.java
+++ src/main/java/com/devh/payment_sim/repository/WalletRepository.java
@@ -3,10 +3,18 @@ package com.devh.payment_sim.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devh.payment_sim.model.Wallet;
+
+import jakarta.persistence.LockModeType;
 
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
     Optional<Wallet> findByUpiHandle(String upiHandle);
     boolean existsByUpiHandle(String upiHandle);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT w FROM Wallet w WHERE w.upiHandle = :upiHandle")
+    Optional<Wallet> findByUpiHandleWithLock(String upiHandle);
 }
